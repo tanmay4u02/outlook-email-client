2e61e6f9c1ac2a73aec3e3a1f0bd87d08427e45c
tanmay4u02 <tps.shinde@gmail.com>
2024-10-14 10:57:17 +0530

    tailwind and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EmailsHome from './pages/EmailsHome';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <EmailsHome />;
 }
 
 export default App;
---
+++ src/pages/EmailsHome.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EmailsHome = () => {
+  return <div>EmailsHome</div>;
+};
+
+export default EmailsHome;
